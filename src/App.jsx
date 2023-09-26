71d92ebc2d9f9a207a49aeb451a62ea65805c977
dilshad360 <dilshad360.dev@gmail.com>
2023-09-26 14:17:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+// import nsslogo from "./assets/nss_logo.png"
+import { Button, Input } from "@material-tailwind/react";
+import Airtable  from "airtable";
+import backendUrl from "./const/backendUrl";
+import { useState } from "react";
+
+const base = new Airtable({ apiKey: `${backendUrl.secretKey}` }).base(
+  `${backendUrl.airtableBase}`
+);
+
+
+
+
+function App() {
+
+
+
+  const [searchText,setSearchText] = useState("");
+
+  const getStudent = async (search) => {
+
+    base("Students")
+      .select({
+        view: "Data",
+        filterByFormula: `({CHEST_NO} = '${search}')`,
+      })
+      .eachPage(
+        (record, fetchNextPage) => {
+          // setResult(records);
+          console.log(record)
+          // setHeading(search);
+          fetchNextPage();
+        },
+        function done(err) {
+          if (err) {
+            console.error(err);
+            return;
+          }
+        }
+      );
+  };
+
+  return (
+    <div className="flex flex-col items-center px-4">
+      {/* <img src={nsslogo} alt="nss_logo" /> */}
+      <div className="py-4">
+      <h4 className="text-2xl font-bold">NSS Admission</h4>
+      <h5 className="text-1xl font-semibold">Score System</h5>
+      </div>
+      <div className="w-full flex flex-col gap-2">
+      <Input type="text" label="Chest No*" onChange={(e) => {setSearchText(e.target.value)}} />
+      <Button className="w-full bg-blue-900" onClick={()=>{getStudent(searchText)}} >Search</Button>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/const/backendUrl.jsx
@@ -0,0 +1,5 @@
+const backendUrl = {
+    airtableBase: import.meta.env.VITE_AIRTABLE_BASE,
+    secretKey: import.meta.env.VITE_SECRET_KEY
+}
+export default backendUrl;
